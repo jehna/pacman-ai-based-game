68e87460a0cbf0f06ff7555dd62da368edfcc6da
jehna <jesse.luoto@gmail.com>
2015-10-31 15:39:30 +0200

    Added: Initial enemy AI

---
+++ Assets/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	User user;
+	Vector2 currentDirection = Vector2.zero;
+
+	/*Vector2[] possibleMovements = new[] {
+		new Vector2(-1,-1), new Vector2( 0,-1), new Vector2( 1,-1),
+	    new Vector2(-1, 0),                     new Vector2( 1, 0),
+		new Vector2(-1, 1), new Vector2( 0, 1), new Vector2( 1, 1)
+	};*/
+
+	Vector2[] possibleMovements = new[] {
+		                    new Vector2( 0,-1),
+	    new Vector2(-1, 0),                     new Vector2( 1, 0),
+		                    new Vector2( 0, 1)
+	};
+
+	// Use this for initialization
+	void Start () {
+		user = FindObjectOfType<User> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Move() {
+		// Check if we can go to current dir
+		if (currentDirection != Vector2.zero) {
+			if (!Physics.Linecast(transform.position, transform.position + (Vector3)currentDirection, LayerMask.NameToLayer("wall"))) {
+				transform.Translate(currentDirection);
+				return;
+			}
+		}
+
+		// Check the next dir
+		float closest = Mathf.Infinity;
+		Vector2 move = Vector2.zero;
+		foreach (Vector2 movement in possibleMovements) {
+			// Cannot change 180 degrees
+			if (movement == -currentDirection) continue;
+
+			// Cannot go this way
+			if (Physics.Linecast(transform.position, transform.position + (Vector3)movement, LayerMask.NameToLayer("wall"))) continue;
+
+			float currDistance = ((Vector2)user.transform.position - ((Vector2)transform.position + movement)).SqrMagnitude();
+			if (currDistance < closest) {
+				move = movement;
+				closest = currDistance;
+			}
+		}
+
+		currentDirection = move;
+		transform.Translate (move);
+	}
+}
---
+++ Assets/User.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class User : MonoBehaviour {
+
+	public enum SwipeDirection {
+		Up,
+		Down,
+		Right,
+		Left,
+		None
+	}
+
+	void Start() {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		SwipeDirection currSwipeDir = GetCurrentSwipeDirection ();
+		if (currSwipeDir != SwipeDirection.None) {
+			Vector2 move = Vector2.zero;
+			switch(currSwipeDir) {
+				case SwipeDirection.Down:
+					move = Vector2.down;
+					break;
+				case SwipeDirection.Up:
+					move = Vector2.up;
+					break;
+				case SwipeDirection.Left:
+					move = Vector2.left;
+					break;
+				case SwipeDirection.Right:
+					move = Vector2.right;
+					break;
+			}
+
+			this.transform.Translate(move);
+			
+			foreach (Enemy enemy in FindObjectsOfType<Enemy>()) {
+				enemy.Move();
+			}
+		}
+	}
+
+	SwipeDirection GetCurrentSwipeDirection() {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) return SwipeDirection.Up;
+		if (Input.GetKeyDown(KeyCode.DownArrow)) return SwipeDirection.Down;
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) return SwipeDirection.Left;
+		if (Input.GetKeyDown(KeyCode.RightArrow)) return SwipeDirection.Right;
+
+		if (Input.touchCount == 0) return SwipeDirection.None;
+
+		Touch touch = Input.GetTouch(0);
+
+		if (touch.phase != TouchPhase.Ended) return SwipeDirection.None;
+
+		if (Mathf.Abs (touch.deltaPosition.x) > Mathf.Abs (touch.deltaPosition.y)) {
+			return touch.deltaPosition.x > 0 ? SwipeDirection.Right : SwipeDirection.Left;
+		} else {
+			return touch.deltaPosition.y > 0 ? SwipeDirection.Up : SwipeDirection.Down;
+		}
+	}
+
+
+}
